b5941f24061dba338052e8917682b1d5147d1fe1
Alireza Jalali <alirezajalali.dev@gmail.com>
2023-11-22 18:41:13 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-function myFunction() {
+function Menu() {
     let icon = document.getElementById("main-nav");
     if (icon.className === "nav") {
         icon.className += " responsive";
     } else {
         icon.className = "nav";
+    }
+}
+
+function darkMode() {
+    let button = document.getElementById("darkmode");
+    if (button.className === "") {
+        button.className += "darkmode";
+    } else {
+        button.className = "";
     }
 }
